8044ba439f9120756501a0dc6b73cb9bda6673cc
Harshal Madgulkar <harshal.madgulkar725@gmail.com>
2023-05-01 06:55:10 +0530

    Final Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# BackendTest-NodejsAuthentication
- 
---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://aniketrec11:<password>@cluster0.2eg6qld.mongodb.net/?retryWrites=true&w=majority",
+}
